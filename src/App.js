d909c6698cf70b1852d6d93f17435fab21e71e10
lakshmi1809shree <lakshmi1809shree@gmail.com>
2021-09-07 12:33:03 +0530

    init msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import React,{Component} from 'react';
+
 import logo from './logo.svg';
 import './App.css';
+import DogList from './DogList';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dogs:[]
+    }
+  }
+//this a react life cycle method ...
+  componentDidMount() {
+    fetch("https://dog.ceo/api/breeds/image/random/10")
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({dogs: data.message})
+      })
+  }
+  render() {
+    return (
+      <div>
+        <h1 style={{ textAlign: 'center' }}>
+          Welcome to Dog World
+        </h1>
+        <DogList   dogs={this.state.dogs}   />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/DogList.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Dog from './Dog'
+import'./DogList.css'
+
+const DogList = (props) => {
+    const dogsArray = props.dogs.map((dogURL) => {
+        return <Dog url={dogURL}/>
+    })
+
+
+    return (
+        <div className="container">
+            {dogsArray}
+        </div>
+    )
+}
+
+export default DogList;
